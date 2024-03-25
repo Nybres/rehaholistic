52a4cfd6b494da928406301bd388d36c693010b3
Nybres <pawelserbyn@wp.pl>
2024-03-25 18:00:56 +0100

    strona kontakt

--- wp-content/themes/rehaholistic/resources/views/contact.blade.php
+++ wp-content/themes/rehaholistic/resources/views/contact.blade.php
@@ -12,32 +12,94 @@
                 <p class="heading__subtitle">Umów się na wizytę już dziś!</p>
             </div>
             <div class="contact__row">
-                <div class="contact__card">
-                    <div class="contact__card-left">
-                        <div>
-                            <h3>Radosław Meller</h3>
-                            <p>ul.Lotnicza 25B/2</p>
+                @if ($groupOne = get_field('kontakt_pierwszy'))
+                    <div class="contact__card">
+                        <div class="contact__card-left">
+                            <div>
+                                <h3 class="h2 fw-500 contact__card-title mb--8">{{ $groupOne['imie_i_nazwisko'] }}</h3>
+                                <p>{{ $groupOne['adres'] }}</p>
+                            </div>
+                            <div>
+                                <h4 class="h3 mb--8 fw-500">Godziny Przyjęć</h4>
+                                <p>{{ $groupOne['godziny_przyjec'] }}</p>
+                            </div>
+                            <div>
+                                <h4 class="h3 mb--8 fw-500">Dane kontakowe</h4>
+                                <a href="tel:{{ $groupOne['telefon'] }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone">
+                                        <path
+                                            d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
+                                        </path>
+                                    </svg>
+                                    {{ $groupOne['telefon'] }}
+                                </a>
+                                <a href="mailto:{{ $groupOne['email'] }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
+                                        <path
+                                            d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
+                                        </path>
+                                        <polyline points="22,6 12,13 2,6"></polyline>
+                                    </svg>
+                                    {{ $groupOne['email'] }}
+                                </a>
+                            </div>
                         </div>
-                        <div>
-                            <h4>Godziny Przyjęć</h4>
-                            <p>sfsfas sasafasf safsaasf</p>
-                        </div>
-                        <div>
-                            <h4>Dane kontakowe</h4>
-                            <a href="tel:+">
-                                sfasfasfsafsfasaf
-                            </a>
-                            <a href="mailto:">
-                                sfasafsfasafsafsfasfa
-                            </a>
+                        <div class="contact__card-right">
+                            <picture>
+                                <img loading="lazy" width="240" height="180" src="{{ $groupOne['zdjecie']['url'] }}"
+                                    alt="{{ $groupOne['zdjecie']['alt'] }}">
+                            </picture>
                         </div>
                     </div>
-                    <div class="contact__card-right">
-                        <picture>
-                            <img src="" alt="">
-                        </picture>
+                @endif
+                @if ($groupOne = get_field('kontakt_drugi'))
+                    <div class="contact__card">
+                        <div class="contact__card-left">
+                            <div>
+                                <h3 class="h2 fw-500 contact__card-title mb--8">{{ $groupOne['imie_i_nazwisko'] }}</h3>
+                                <p>{{ $groupOne['adres'] }}</p>
+                            </div>
+                            <div>
+                                <h4 class="h3 mb--8 fw-500">Godziny Przyjęć</h4>
+                                <p>{{ $groupOne['godziny_przyjec'] }}</p>
+                            </div>
+                            <div>
+                                <h4 class="h3 mb--8 fw-500">Dane kontakowe</h4>
+                                <a href="tel:{{ $groupOne['telefon'] }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone">
+                                        <path
+                                            d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z">
+                                        </path>
+                                    </svg>
+                                    {{ $groupOne['telefon'] }}
+                                </a>
+                                <a href="mailto:{{ $groupOne['email'] }}">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                                        viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
+                                        <path
+                                            d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z">
+                                        </path>
+                                        <polyline points="22,6 12,13 2,6"></polyline>
+                                    </svg>
+                                    {{ $groupOne['email'] }}
+                                </a>
+                            </div>
+                        </div>
+                        <div class="contact__card-right">
+                            <picture>
+                                <img loading="lazy" width="240" height="180" src="{{ $groupOne['zdjecie']['url'] }}"
+                                    alt="{{ $groupOne['zdjecie']['alt'] }}">
+                            </picture>
+                        </div>
                     </div>
-                </div>
+                @endif
             </div>
         </div>
     </section>
